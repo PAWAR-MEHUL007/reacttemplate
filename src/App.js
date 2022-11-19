84f0480c0a512afbfa6e560b1e59843d864838b3
PAWAR-MEHUL007 <mpavar007@gmail.com>
2022-11-19 17:05:07 +0530

    All pages

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./Header"; //Include Header
+import Footer from "./Footer";
+import Home from "./Home";
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Home></Home>
+      <Footer></Footer>
+    </div>
+  );
+}
+export default App;
